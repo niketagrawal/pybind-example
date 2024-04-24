0303049ca0683c97ae78a02c7e267ae9b3d87148
Niket <15953349+niketagrawal@users.noreply.github.com>
2024-04-24 16:07:01 +0200

    import . leads to circular import error. When package is installed using pip install ., _core is accessible directly inside it. Mentioning from . import _core means we import the package again and from it import _core which leads to circular import error

--- python/pybind_project_example/__init__.py
+++ python/pybind_project_example/__init__.py
@@ -1,10 +1,5 @@
 from __future__ import annotations
 
-from pybind_project_example import _core
-
-__doc__ = _core.__doc__
-__version__ = _core.__version__
-add = _core.add
-subtract = _core.subtract
+from _core import add, subtract, __version__, __doc__
 
 __all__ = ["__doc__", "__version__", "add", "subtract"]
